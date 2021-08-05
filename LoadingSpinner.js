a09a3f5784746ac2293b1b18de0b109be23fe5fe
Raj Aryan <73771884+rajaryan18@users.noreply.github.com>
2021-08-06 00:08:09 +0530

    Loading Spinner (css+js)
    Loading spinner for displaying when the page takes time to update.

---
+++ LoadingSpinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './LoadingSpinner.css';
+
+const LoadingSpinner = props => {
+  return (
+    <div className={`${props.asOverlay && 'loading-spinner__overlay'}`}>
+      <div className="lds-dual-ring"></div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
